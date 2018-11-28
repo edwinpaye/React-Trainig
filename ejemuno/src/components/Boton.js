66deb973f1f01b099b8d5296acc0ec1bfda45deb
edwinpaye <baratheonts@gmail.com>
2018-11-27 23:03:28 -0400

    fix function sumar

--- ejemuno/src/components/Boton.js
+++ ejemuno/src/components/Boton.js
@@ -13,17 +13,17 @@ class Boton extends React.Component{
 
     // sumar = () => this.setState({...this.state, count: this.state.count+1})
 
-    sumar = () => this.setState(state => ({count: state.count + 1}))
+    // sumar = () => this.setState(state => ({count: state.count + 1}))
 
-    // sumar() {
-    //     return () => this.setState(state => ({count: state.count + 1}))
-    // }
+    sumar() {
+        return () => this.setState(state => ({count: state.count + 1}))
+    }
 
-    resta = () => this.setState(state => ({count: state.count - 1}))
+    // resta = () => this.setState(state => ({count: state.count - 1}))
 
-    // resta(){
-    //     return () => this.setState(state => ({count: state.count - 1}))
-    // }
+    resta(){
+        return () => this.setState(state => ({count: state.count - 1}))
+    }
 
     // resetear = () => this.setState(state => ({count: 0}))
 
@@ -42,8 +42,8 @@ class Boton extends React.Component{
         return(
             <div>
                 <h1>{this.contador()}</h1>
-                <button onClick={this.sumar}>Incrementar</button>
-                <button onClick={this.resta}>Reducir</button>
+                <button onClick={this.sumar()}>Incrementar</button>
+                <button onClick={this.resta()}>Reducir</button>
                 <button onClick={this.resetear()}>Resetear</button>
                 {/* <h1>{this.props.aaa}</h1> */}
             </div>
