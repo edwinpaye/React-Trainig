f35b6ca2e062ccf038afbf111eba4244bf2b78f1
edwinpaye <baratheonts@gmail.com>
2019-01-23 10:33:56 -0400

    fix methods

--- practjs/PractVariables.js
+++ practjs/PractVariables.js
@@ -1,8 +1,8 @@
 /*Una convecion es: Usar el guion o doble guion bajo para indicar que
 algo es privado*/
 
-// var Beer = function(name) {
-//     this.__name = name || "unnamed";
+// var Variable = function(name) {
+//     this.__name = name || "sin nombre";
 //     this.setName = function(newname) {
 //         if (newname) {
 //             this.__name = newname;
@@ -13,12 +13,12 @@ algo es privado*/
 //     }
 // }
 
-// var beer = new Beer("Punk IPA");
-// console.log(beer.getName());        // Imprime Punk IPA
-// beer.setName();
-// console.log(beer.getName());        // Imprime Punk IPA
-// beer.__name = undefined;
-// console.log(beer.getName());        // Imprime undefined
+// var variable = new Variable("variable uno");
+// console.log(variable.getName());        // Imprime variable uno
+// variable.setName();
+// console.log(variable.getName());        // Imprime variable uno
+// variable.__name = undefined;
+// console.log(variable.getName());        // Imprime undefined
 
 
 /*Por defecto si no devolvemos nada, las funciones en JavaScript
@@ -27,8 +27,8 @@ devuelven el propio objeto que se está creando (y que se referencia
 por this). Pero nada impide devolver otro objeto que contenga
 tan solo los miembros públicos:*/
 
-var Beer = function(name) {
-    var __name = name || "unnamed";
+var Variable = function(name) {
+    var __name = name || "sin nombre";
     var setName = function(newname) {
         if (newname) {
             __name = newname;
@@ -44,14 +44,15 @@ var Beer = function(name) {
     }
 }
 
-var beer = new Beer("Punk IPA");
-var beer2 = new Beer("Hoegaarden");
-var beer3 = new Beer();
-beer2.__name = "Sang de rabo de drac";
-beer2.setName("Devil's IPA");
-console.log(beer3.getName());
-console.log(beer3.__name);
-// console.log(beer.getName());    // Imprime Punk IPA
-// console.log(beer2.getName());   // Imprime Devil's IPA
-// console.log(beer.__name);       // Imprime undefined
-// console.log(beer2.__name);      // Imprime Sang de rabo de drac
+var variable = new Variable("variable uno");
+var variable2 = new Variable("variable dos");
+var variable3 = new Variable();
+variable2.__name = "change __name";
+variable2.setName("change var 2");
+console.log(variable3.getName());
+console.log(variable3.__name);
+
+console.log(variable.getName());    // Imprime variable uno
+console.log(variable2.getName());   // Imprime change dos
+console.log(variable.__name);       // Imprime undefined
+console.log(variable2.__name);      // Imprime change __name
